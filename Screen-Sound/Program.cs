f4e695cecbeb18d5d3ef345eba5978a5e52f3ee4
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-13 17:05:37 -0300

    feat: adding rateBand function

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Screen Sound
-List<string> bandsList = new List<string> { "AC/DC", "Nirvana", "Gun's in Roses" };
+//List<string> bandsList = new() { "AC/DC", "Nirvana", "Gun's in Roses" };
+
+Dictionary<string, List<double>> bandsDictionary = new();
 
 void ShowMessage()
 {
@@ -40,7 +42,7 @@ void ShowOptions()
             break;
 
         case 3:
-            Console.WriteLine("You want rate a band");
+            RateBand();
             break;
 
         case 4:
@@ -62,10 +64,10 @@ void ShowOptions()
 void RegisterBand()
 {
     Console.Clear();
-    title("Band's Register");
+    Title("Band's Register");
     Console.Write("Type the band's name do you want register: ");
     string bandName = Console.ReadLine()!;
-    bandsList.Add(bandName);
+    bandsDictionary.Add(bandName, new List<double>());
     Thread.Sleep(2000);
     Console.WriteLine($"The band {bandName} was succesfully registered");
     Thread.Sleep(2000);
@@ -76,12 +78,12 @@ void RegisterBand()
 void ShowAllBands()
 {
     Console.Clear();
-    title("Show All Bands");
-    if (bandsList.Count <= 0)
+    Title("Show All Bands");
+    if (bandsDictionary.Count <= 0)
     {
         Console.WriteLine("Not have any Bands Registered!");
-        Console.WriteLine("Returning to menu in 5 secconds");
-        Thread.Sleep(5000);
+        Console.WriteLine("Returning to menu in 3 secconds");
+        Thread.Sleep(3000);
         Console.Clear();
         ShowOptions();
     } else
@@ -90,18 +92,18 @@ void ShowAllBands()
         //{
         //    Console.WriteLine($"Band's Name: {bandsList[i]}");
         //}
-        foreach (string bandName in bandsList)
+        foreach (string bandName in bandsDictionary.Keys)
         {
             Console.WriteLine($"Band's Name: {bandName}");
         }
-        Console.WriteLine("Type any key to return to menu");
+        Console.WriteLine("\nType any key to return to menu");
         Console.ReadKey();
         Console.Clear();
         ShowOptions();
     }
 }
 
-void title(string title)
+void Title(string title)
 {
     int qntTitle = title.Length;
     string symbols = string.Empty.PadLeft(qntTitle, '*');
@@ -109,6 +111,34 @@ void title(string title)
     Console.WriteLine(symbols);
     Console.WriteLine(title);
     Console.WriteLine(symbols + "\n");
+}
+
+void RateBand()
+{
+    Console.Clear();
+    Title("Rate a Band");
+
+    Console.Write("Type a band's name to rate: ");
+    string bandNameRate = Console.ReadLine()!;
+
+    if (bandsDictionary.ContainsKey(bandNameRate))
+    {
+        Console.Write($"Type a number to rate {bandNameRate}: ");
+        double bandRateUser = Double.Parse(Console.ReadLine()!);
+        bandsDictionary[bandNameRate].Add(bandRateUser);
+        Console.WriteLine($"You typed {bandRateUser} to {bandNameRate}\n");
+        Console.WriteLine("Returning to menu in 3 secconds");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ShowOptions();
+    } else
+    {
+        Console.WriteLine($"The band {bandNameRate} wasn't register");
+        Console.WriteLine("Returning to menu in 3 secconds");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ShowOptions();
+    }
 }
 
 ShowOptions();
