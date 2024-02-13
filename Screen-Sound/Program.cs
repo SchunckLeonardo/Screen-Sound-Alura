ad73f96eb11884c5ae6b897a52d097152186530f
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-13 13:15:11 -0300

    feat: adding switch case to user options

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -17,8 +17,42 @@ void ShowOptions()
     Console.WriteLine("1 - Register a band");
     Console.WriteLine("2 - Show all bands");
     Console.WriteLine("3 - Rate a band");
-    Console.WriteLine("4 - Show band media");
+    Console.WriteLine("4 - Show bands media");
     Console.WriteLine("5 - Exit Program");
+
+    Console.Write("\nType your option: ");
+    string userOption = Console.ReadLine()!;
+
+    int userOptionInt = int.Parse(userOption);
+
+    switch (userOptionInt)
+    {
+        case 1: 
+            Console.WriteLine("You want register a band");
+            break;
+
+        case 2:
+            Console.WriteLine("You want show all bands");
+            break;
+
+        case 3:
+            Console.WriteLine("You want rate a band");
+            break;
+
+        case 4:
+            Console.WriteLine("You want show band media");
+            break;
+
+        case 5:
+            Console.WriteLine("See you later!");
+            break;
+
+        default:
+            Console.WriteLine("The option " + userOptionInt + " there isn't in Options List");
+            Console.WriteLine("Type again!");
+            break;
+
+    }
 }
 
 ShowMessage();
